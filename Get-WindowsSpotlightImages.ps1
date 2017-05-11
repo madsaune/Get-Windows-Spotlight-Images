95a0deca29ad943b4ecd77e6310f71665c0694c9
Mads Aune <madsaune@gmail.com>
2017-05-11 16:12:44 +0200

    Create Get-WindowsSpotlightImages.ps1

---
+++ Get-WindowsSpotlightImages.ps1
@@ -0,0 +1,30 @@
+Function Get-SpotlightFiles {
+    If(-not (Test-Path "$env:USERPROFILE\Pictures\Spotlight")) {
+        New-Item "$env:USERPROFILE\Pictures\Spotlight" -ItemType Directory
+    }
+    Get-ChildItem "C:\Users\mads.aune\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets" | ForEach-Object {
+        $NewFilename = $_.Name + ".jpeg"
+        # If(Test-Path (Join-Path "$env:USERPROFILE\Pictures\Spotlight" $NewFilename)) {
+        #     $NewFilename = $_.Name + "(2).jpeg"
+        # }
+        Copy-Item $_.Fullname (Join-Path "$env:USERPROFILE\Pictures\Spotlight" $NewFilename)
+    }
+}
+
+Function Remove-UnwantedSpotlightImages {
+    $ImagesToMove = @()
+    $AllImages = Get-ChildItem "$env:USERPROFILE\Pictures\Spotlight\*.jpeg"
+    $AllImages | ForEach-Object {
+        $image = New-Object System.Drawing.Bitmap $_.FullName
+        If($image.Width -ne 1920) {
+            $ImagesToMove += $_.FullName
+        }
+        $image.Dispose()
+    }
+
+    $ImagesToMove | Remove-Item
+}
+
+Add-Type -AssemblyName System.Drawing
+Get-SpotlightFiles
+Remove-UnwantedSpotlightImages
